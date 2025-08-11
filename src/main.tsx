d3bf0ce0d5fa4aa540c3f0bc9820d9bbe868696d
Dennis Domingo <dennis@Mac.attlocal.net>
2025-08-11 10:28:44 -0700

    Correct production setup for TailWind

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import AppableLandingModernV3 from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
